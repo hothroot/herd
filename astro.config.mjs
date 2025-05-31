26ac2715fc6082da8790f23f49b6909f8f48cbee
Chris Wren <chris@drwren.com>
2025-05-31 15:21:54 -0400

    add client side javascript
    add preact, remove react (inherited from other template)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [preact()],
   site: "https://astroherd.netlify.app/",
 });
