96096e00b4f7011700e5d7d0133a6847b1676bfc
Chris Wren <chris@drwren.com>
2025-06-02 10:25:00 -0400

    add tailwindcss
    seems like it is required for shadcn
    
    also repair all the damage tailwindcss reset did
    to the default styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
   site: "https://astroherd.netlify.app/",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
