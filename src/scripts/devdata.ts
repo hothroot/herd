ddbbad6c6a94ff1cb2baeb33251c45552dcacb26
Chris Wren <chris@drwren.com>
2025-06-04 15:58:46 -0400

    hack to avoid abusing the API during development
    TODO: rework this to allow testing of error states

---
+++ src/scripts/devdata.ts
@@ -0,0 +1,69 @@
+const ok: Record<string, any> = {
+    'location': 'Arlington',
+    'lowAccuracy': 'false',
+    'isSplit': 'false',
+    'state': 'MA',
+    'district': '5',
+    'representatives': [
+        {
+            'id': 'C001101',
+            'name': 'Katherine Clark',
+            'phone': '202-225-2836',
+            'url': 'https://katherineclark.house.gov',
+            'photoURL': 'https://images.5calls.org/house/256/C001101.jpg',
+            'party': 'Democrat',
+            'state': 'MA',
+            'reason': 'This is your representative in the House.',
+            'area': 'US House',
+            'field_offices': [
+                {
+                    'phone': '617-354-0292',
+                    'city': 'Malden'
+                },
+            ]
+        },
+        {
+            'id': 'M000133',
+            'name': 'Ed Markey',
+            'phone': '202-224-2742',
+            'url': 'https://www.markey.senate.gov',
+            'photoURL': 'https://images.5calls.org/senate/256/M000133.jpg',
+            'party': 'Democrat',
+            'state': 'MA',
+            'reason': 'This is one of your two Senators.',
+            'area': 'US Senate',
+            'field_offices': [
+                {
+                    'phone': '413-785-4610',
+                    'city': 'Springfield'
+                },
+                {
+                    'phone': '617-565-8519',
+                    'city': 'Boston'
+                }
+            ]
+        },
+        {
+            'id': 'W000817',
+            'name': 'Elizabeth Warren',
+            'phone': '202-224-4543',
+            'url': 'https://www.warren.senate.gov',
+            'photoURL': 'https://images.5calls.org/senate/256/W000817.jpg',
+            'party': 'Democrat',
+            'state': 'MA',
+            'reason': 'This is one of your two Senators.',
+            'area': 'US Senate',
+            'field_offices': [
+                {
+                    'phone': '413-788-2690',
+                    'city': 'Springfield'
+                },
+                {
+                    'phone': '617-565-3170',
+                    'city': 'Boston'
+                }
+            ]
+        },
+    ]
+}
+export default ok;
