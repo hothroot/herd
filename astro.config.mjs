fda0c6622e5f88d57d49d85c7c7caa830b893dd6
Chris Wren <chris@drwren.com>
2025-06-05 20:30:17 -0400

    enable netlify adaptor for staging
    https://docs.astro.build/en/guides/on-demand-rendering/

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,23 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   site: "https://astroherd.netlify.app/",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   env: {
     schema: {
       STAGING: envField.boolean({ context: "server", access:"public", default: false }),
       FIVECALLS_API: envField.string({ context: "server", access:"secret" }),
     }
   },
+
+  adapter: netlify(),
 });
