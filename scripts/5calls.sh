ce867f7344c5b52dc6e1f5dde0e243a5ced5f152
Chris Wren <chris@drwren.com>
2025-07-29 15:29:15 -0400

    take address on command line, pretty print
    requires python and bash, should be std

--- scripts/5calls.sh
+++ scripts/5calls.sh
@@ -2,5 +2,15 @@
 
 . .env
 
-curl -G https://api.5calls.org/v1/representatives?location=41%20Union%20St%20Boston%20MA%2002108 \
-  -H "X-5Calls-Token: $FIVECALLS_API" 
+ADDRESS="41 Union St.;Boston, MA 02108"
+
+if [ $# -gt 0 ]
+then
+    ADDRESS=$1
+fi
+
+echo "Looking up: " $ADDRESS
+
+ENCODED=$(echo $ADDRESS | sed -e "s/ /%20/g;s/;/%3B/g;s/,/%2C/g")
+curl -G https://api.5calls.org/v1/representatives?location=${ENCODED} \
+  -H "X-5Calls-Token: ${FIVECALLS_API}" | python -m json.tool
