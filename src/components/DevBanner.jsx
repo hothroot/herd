41b285af1df6f0c3df24615d927bd027b4cbb9dc
Chris Wren <chris@drwren.com>
2025-09-10 16:34:31 -0400

    add a dark launch mode
    update staging banner to make it explicit that
    letters will not be delivered.
    
    reverse sense of conditionals to make sure dev
    overrides staging overrides dark

--- src/components/DevBanner.jsx
+++ src/components/DevBanner.jsx
@@ -1,20 +1,29 @@
 
 const isDev = import.meta.env.DEV;
 const isStaging = import.meta.env.STAGING;
+const isDarkLaunch = import.meta.env.DARKLAUNCH;
 
 export default function DevBanner() {
-    if (isStaging) {
-        return (
-            <div id="butterbar">
-                This site is under development, please visit&nbsp;
-                <a href="https://herdonthehill.org/">the real site</a> instead. 
-            </div>
-        );
-    } else if (isDev) {
+    if (isDev) {
         return (
             <div id="bloodbar">
                 Dev Site
             </div>
         );
-    }
+    } else if (isStaging) {
+        return (
+            <div id="butterbar">
+                Letters created on this site will not be delivered!, please visit&nbsp;
+                <a href="https://www.herdonthehill.com/">the real site</a> instead. 
+            </div>
+        );
+    } else if (isDarkLaunch) {
+        return (
+            <div id="graybar">
+                This site is under development,&nbsp;
+                <a href="mailto:webmaster@herdonthehill.com">report any issues</a>&nbsp;
+                you may experience. 
+            </div>
+        );
+    } 
 }
