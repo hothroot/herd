2e46e802418bffa4aa97f1244a991ca5ebf64071
Chris Wren <chris@drwren.com>
2025-09-11 13:01:44 -0400

    optimize images
    - webp
    - right-sized
    - explicit sizes
    
    TODO: get the sizes from the images during build

--- src/components/PhotoCarousel.jsx
+++ src/components/PhotoCarousel.jsx
@@ -12,13 +12,13 @@ const PhotoCarousel = () => {
     <Carousel className="w-full max-w-lg">
       <CarouselContent>
         <CarouselItem key="1">
-            <img src="/home-carousel-3.jpg"></img>
+            <img src="/home-carousel-3.webp" width="512" height="256"></img>
         </CarouselItem>
         <CarouselItem key="2">
-            <img src="/home-carousel-2.jpg"></img>
+            <img src="/home-carousel-2.webp" width="512" height="256"></img>
         </CarouselItem>
         <CarouselItem key="4">
-            <img src="/home-carousel-4.jpg"></img>
+            <img src="/home-carousel-4.webp" width="512" height="256"></img>
         </CarouselItem>
       </CarouselContent>
       <CarouselPrevious className="relative left-0 translate-x-0"/>
