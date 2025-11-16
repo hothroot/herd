ac9b9b3ce0ef415300a039ba19c61e0a407ec72e
Chris Wren <c@drwren.com>
2025-11-16 18:07:12 -0500

    add a flag to change order of address validators

--- src/scripts/address-validate.js
+++ src/scripts/address-validate.js
@@ -2,6 +2,7 @@ import { USPSValidator } from "@/scripts/address-usps"
 import { MapsValidator } from "@/scripts/address-maps"
 import { AddressStatus } from '@/scripts/letter-state'
 import { allStateNamesAndCodes, zipRegExp } from "@/scripts/states";
+import { USPS_FIRST } from "astro:env/server";
 
 export async function validateAddress(address) {
     if (!address.name
@@ -15,9 +16,18 @@ export async function validateAddress(address) {
         address.notes = "Address is missing required information.";
         return address;
     }
-    var result = await new USPSValidator().validateAddress(address);
+    const usps = new USPSValidator();
+    const maps = new MapsValidator();
+    var validators = [ usps ];
+    if (USPS_FIRST) {
+        validators.push(maps);
+    } else {
+        validators.unshift(maps);
+    }
+
+    var result = await validators[0].validateAddress(address);
     if (result.status == AddressStatus.QUOTA || result.status == AddressStatus.ERROR) {
-        result = await new MapsValidator().validateAddress(address);
+        result = await validators[0].validateAddress(address);
     }
     return result;
 }
